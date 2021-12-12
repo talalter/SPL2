47b6c4579288385f1b7423dd91b78c9578c7c95b
talalter <talalter95900@gmail.com>
2021-12-12 19:15:28 +0200

    initial

--- src/main/java/bgu/spl/mics/MessageBusImpl.java
+++ src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -84,7 +84,7 @@ public class MessageBusImpl implements MessageBus {
 			service_message.get(microservice).add(e);
 			Future<T> f = new Future<>();
 			message_future.put(e, f);
-			notifyAll();
+			//notifyAll();
 			return f;
 		}
 		return null;
@@ -108,10 +108,9 @@ public class MessageBusImpl implements MessageBus {
 	}
 
 	@Override
-	public Message awaitMessage(MicroService m) throws InterruptedException {
-	//	System.out.println(service_message.get(m).isEmpty());
-	//	while(service_message.get(m).isEmpty())
-	//		wait();
+	public synchronized Message awaitMessage(MicroService m) throws InterruptedException {
+		while(service_message.get(m).isEmpty())
+			wait();
 		Vector<Message> tempService = service_message.get(m);
 		Message output=tempService.firstElement();
 		service_message.get(m).remove(output);
--- src/main/java/bgu/spl/mics/application/messages/TrainModelEvent.java
+++ src/main/java/bgu/spl/mics/application/messages/TrainModelEvent.java
@@ -1,7 +1,11 @@
 package bgu.spl.mics.application.messages;
 
+import bgu.spl.mics.Event;
 import bgu.spl.mics.Message;
+import bgu.spl.mics.application.objects.Model;
 
-public class TrainModelEvent implements Message {
-
+public class TrainModelEvent implements Event<Model> {
+    public TrainModelEvent(){
+        System.out.println("TRAIN MODEL EVENT CONSTRUCTOR");
+    }
 }
