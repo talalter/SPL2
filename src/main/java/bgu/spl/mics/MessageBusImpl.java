719086f2caa988911692c0fa1a1e763bac297fb3
talalter <talalter95900@gmail.com>
2021-12-13 18:21:48 +0200

    initial

--- src/main/java/bgu/spl/mics/MessageBusImpl.java
+++ src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -3,7 +3,7 @@ package bgu.spl.mics;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Vector;
-
+//.
 /**
  * The {@link MessageBusImpl class is the implementation of the MessageBus interface.
  * Write your implementation here!
